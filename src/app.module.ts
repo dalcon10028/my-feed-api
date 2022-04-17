456678100980214c6ab912e2cf934a3a015ac7d2
yeon <dalcon10280@gmail.com>
2022-04-18 00:10:31 +0900

    chore: mysql 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FeedsModule } from './feeds/feeds.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [FeedsModule],
+  imports: [TypeOrmModule.forRoot(), FeedsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
