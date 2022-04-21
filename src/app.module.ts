0992294644e497626dfcdd7a09a4c55a87709e6d
dalcon10028 <dalcon10280@gmail.com>
2022-04-21 22:17:35 +0900

    feat: env 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,18 @@ import { AppService } from './app.service';
 import { FeedsModule } from './feeds/feeds.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), FeedsModule, UsersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(),
+    FeedsModule,
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
